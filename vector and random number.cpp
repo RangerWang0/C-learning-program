20bbd5f0271b15b516dd60bf48f4317ab62bb117
RangerWang0 <42091172+RangerWang0@users.noreply.github.com>
2019-05-07 23:28:31 +0800

    test vector and random numbers

---
+++ vector and random number.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include<ctime>
+#include<random>
+#include<vector>
+using namespace std;
+void fill(vector <int>& v, int cnt);
+void test(vector<int> & v);
+int main()
+{
+	vector <int> v;
+	fill(v, 1000);
+	test(v);
+	return 0;
+}
+void fill(vector <int>& v, int cnt)
+{
+	v.clear();
+	for (int i = 0; i <= cnt; i++)
+		v.push_back(rand());
+}
+void test(vector<int> & v)
+{
+	sort(v.begin(), v.end());
+	for (int i = 0; i <= v.size() - 1; i++)
+		if (v[i] > v[i + 1])
+		{
+			cout << "ERROR!" << endl;
+		}
+		else
+		{
+			printf("round %d test success\n", i);
+		}
+}
